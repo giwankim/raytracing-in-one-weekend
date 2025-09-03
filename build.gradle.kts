e010a7d0c5dddd78bd47d736a4ffc90ffb46431c
Giwan Kim <giwan.kim@gmail.com>
2025-09-03 21:46:06 +0900

    feat: add a progress indicator
    - integrate logging with KotlinLogging and Logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ repositories {
 }
 
 dependencies {
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.18")
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/com/giwankim/raytracing/Main.kt
+++ src/main/kotlin/com/giwankim/raytracing/Main.kt
@@ -1,28 +1,35 @@
 package com.giwankim.raytracing
 
+import io.github.oshai.kotlinlogging.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
 fun main() {
     // Image
     val imageWidth = 256
     val imageHeight = 256
 
-    val out = System.out.bufferedWriter()
-    out.appendLine("P3")
-    out.appendLine("$imageWidth $imageHeight")
-    out.appendLine("255")
+    System.out.bufferedWriter().use { out ->
+        out.appendLine("P3")
+        out.appendLine("$imageWidth $imageHeight")
+        out.appendLine("255")
 
-    // Render
-    for (j in 0 until imageHeight) {
-        for (i in 0 until imageWidth) {
-            val r = i.toDouble() / (imageWidth - 1)
-            val g = j.toDouble() / (imageHeight - 1)
-            val b = 0.0
+        // Render
+        for (j in 0 until imageHeight) {
+            logger.info { "Scanlines remaining: ${imageHeight - j}" }
+            for (i in 0 until imageWidth) {
+                val r = i.toDouble() / (imageWidth - 1)
+                val g = j.toDouble() / (imageHeight - 1)
+                val b = 0.0
 
-            val ir = (255.999 * r).toInt()
-            val ig = (255.999 * g).toInt()
-            val ib = (255.999 * b).toInt()
+                val ir = (255.999 * r).toInt()
+                val ig = (255.999 * g).toInt()
+                val ib = (255.999 * b).toInt()
 
-            out.appendLine("$ir $ig $ib")
+                out.appendLine("$ir $ig $ib")
+            }
         }
     }
-    out.flush()
+
+    logger.info { "Done." }
 }
